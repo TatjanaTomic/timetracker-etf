973f38a19ab2af2117aa651f7a5c3d301f1c7018
tatjanatomic <tatjanatomic997@yahoo.com>
2019-07-12 18:16:14 +0200

    Add UserController

--- src/TimeTrackerEtf/Models/UserInputModel.cs
+++ src/TimeTrackerEtf/Models/UserInputModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TimeTrackerEtf.Domain;
 
 namespace TimeTrackerEtf.Models
 {
@@ -10,6 +11,12 @@ namespace TimeTrackerEtf.Models
         public string Name { get; set; }
         public decimal HourRate { get; set;  }
 
+        public void MapTo(User user)
+        {
+            user.Name = Name;
+            user.HourRate = HourRate;
+
+        }
     }
     
 }
