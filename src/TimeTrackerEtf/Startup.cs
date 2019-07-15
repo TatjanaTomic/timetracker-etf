b5f109362a72c1caa94d24240d3f76477a018c01
tatjanatomic <tatjanatomic997@yahoo.com>
2019-07-15 17:48:05 +0200

    Add error handling middleware

--- src/TimeTrackerEtf/Startup.cs
+++ src/TimeTrackerEtf/Startup.cs
@@ -43,6 +43,7 @@ namespace TimeTrackerEtf
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseMiddleware<ErrorHandlingMiddleware>();
 
             app.UseHttpsRedirection();
 
